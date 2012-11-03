2100a58e56b6475a38f40291f93977313f6d210d
Alex Korovyansky <korovyansk@gmail.com>
2012-11-03 09:48:31 +0700

    added stub list to MatchesActivity

--- android/src/ru/omsu/avangard/omsk/MatchesActivity.java
+++ android/src/ru/omsu/avangard/omsk/MatchesActivity.java
@@ -1,13 +1,17 @@
 package ru.omsu.avangard.omsk;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MatchesActivity extends LifecycleLogActivity {
+public class MatchesActivity extends ListActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_matches);
+	    final String[] matches = {"Авангард - Динамо", "Трактор - Авангард", "Ак барс - Авангард"};
+	    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matches);
+	    setListAdapter(adapter);
 	}
 
 }
